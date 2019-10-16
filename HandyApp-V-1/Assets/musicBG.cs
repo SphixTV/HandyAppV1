5defca2e13f9025ec9c0eb62300ca4596d6525dd
Verantor <56546640+Verantor@users.noreply.github.com>
2019-10-16 20:52:33 +0200

    adding music

---
+++ HandyApp-V-1/Assets/musicBG.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicBG : MonoBehaviour
+{
+    private static musicBG instance;
+     void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
