821c039753b82d301264b2b13c0ec14d3253c5da
Verantor <56546640+Verantor@users.noreply.github.com>
2019-10-17 16:42:40 +0200

    Add files via upload

---
+++ HandyApp-V-1/Assets/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PlayerData
+{
+    public int Coins = 123;
+
+
+    public PlayerData(CoinLoader coinLoader)
+    {
+        Coins = coinLoader.Coins;
+       
+        
+}
+}
