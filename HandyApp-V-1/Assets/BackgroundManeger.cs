a4d739a7fed7009bdfffe31a0169a1dcdf35ca2b
Verantor <56546640+Verantor@users.noreply.github.com>
2019-10-17 16:51:18 +0200

    ds

---
+++ HandyApp-V-1/Assets/BackgroundManeger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManeger : MonoBehaviour
+{
+    private static BackgroundManeger instance;
+    public float Coins;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
