fb4ea519167f2ad5f2e6d3a8dbde6a545fd534c2
Verantor <56546640+Verantor@users.noreply.github.com>
2019-10-19 16:38:42 +0200

    sa

---
+++ HandyApp-V-1/Assets/AddCoins.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCoins : MonoBehaviour
+{
+    public int Coins;
+    public Maneger maneger;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == maneger.player1.name)
+        {
+            GetComponent<Coins>().CoinPlus(Coins);
+            Destroy(gameObject);
+        }
+
+    }
+
+}
