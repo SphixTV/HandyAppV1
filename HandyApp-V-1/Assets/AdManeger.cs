015893adb210d52092486f14113c433562f33458
Verantor <56546640+Verantor@users.noreply.github.com>
2019-10-19 11:36:51 +0200

    ads

---
+++ HandyApp-V-1/Assets/AdManeger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+
+public class AdManeger : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int becomeCoins;
+    public void Show()
+    {
+        if (Advertisement.IsReady("video"))
+        {
+            Advertisement.Show("video", new ShowOptions() { resultCallback = HandleAdResult });
+        }
+    }
+    private void HandleAdResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("Ads Finish");
+                GetComponent<Coins>().CoinPlus(becomeCoins);
+                break;
+
+            case ShowResult.Skipped:
+                Debug.Log("Nicht fertig");
+                break;
+
+            case ShowResult.Failed:
+                Debug.Log("Fail");
+                break;
+        }
+    }
+}
---
+++ HandyApp-V-1/Assets/Coins.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+
+
+    public TextMeshProUGUI coins;
+
+    public DataManager dataManager;
+
+    private void Start()
+    {
+        dataManager.Load("player.json");
+
+        coins.text = dataManager.data.coins.ToString();
+    }
+    public void CoinPlus(int coin)
+    {
+        dataManager.data.coins += coin;
+        coins.text = dataManager.data.coins.ToString();
+    }
+    public void CoinMinus(int coin)
+    {
+        dataManager.data.coins -= coin;
+        coins.text = dataManager.data.coins.ToString();
+    }
+    void OnApplicationQuit()
+    {
+        dataManager.Save("player.json");
+
+
+    }
+
+
+
+}
+
+
+    
